876714ee44c01d0006229f16fa9272cae6d488ff
flower1360 <851192902@qq.com>
2023-02-22 09:57:39 +0800

    :yellow_heart: 1140. 石子游戏 II 动态规划

---
+++ src/main/java/subject/year2023/month2/day0222/StoneGameII.java
@@ -0,0 +1,30 @@
+package subject.year2023.month2.day0222;
+
+/**
+ * Author : flower_micro
+ * Date : 2023/02/20
+ * Subject : 1140. 石子游戏 II
+ * Label : 中等 ：动态规划
+ * Describe ：
+ * 爱丽丝和鲍勃继续他们的石子游戏。许多堆石子排成一行，每堆都有正整数颗石子piles[i]。游戏以谁手中的石子最多来决出胜负。
+ * <p>
+ * 爱丽丝和鲍勃轮流进行，爱丽丝先开始。最初，M = 1。
+ * <p>
+ * 在每个玩家的回合中，该玩家可以拿走剩下的前X堆的所有石子，其中1 <= X <= 2M。然后，令M = max(M, X)。
+ * <p>
+ * 游戏一直持续到所有石子都被拿走。
+ * <p>
+ * 假设爱丽丝和鲍勃都发挥出最佳水平，返回爱丽丝可以得到的最大数量的石头。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/stone-game-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class StoneGameII {
+
+    public int stoneGameII(int[] piles) {
+
+        return 0;
+    }
+}
