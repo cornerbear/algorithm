27807215562d45f932fa6602ff4bacd05a631b5f
flower1360 <851192902@qq.com>
2023-04-02 21:33:54 +0800

    :broken_heart: 85. 最大矩形 单调栈

---
+++ src/main/java/subject/year2023/month4/day0402/MaximalRectangle.java
@@ -0,0 +1,13 @@
+package subject.year2023.month4.day0402;
+
+/**
+ * @author : corner_bear
+ * @since : 2023/4/2 10:22
+ * @subject : 85. 最大矩形
+ * @label : 困难 :
+ * @description :
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ */
+
+public class MaximalRectangle {
+}
